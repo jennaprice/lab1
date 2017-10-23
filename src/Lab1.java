122a3e961af6b09ebb738f1d67567988124cd0d2
J.O.Price <notantisocial@users.noreply.github.com>
2017-10-23 14:55:27 -0400

    Initialized the Labs/Labwork Repo

---
+++ src/Lab1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+
+		Scanner scnr = new Scanner(System.in);
+		char continueGame = 'y';
+
+		System.out.println("Welcome to Grand Circus Room Detail Generator!");
+		// checks to continue the cycle
+		while (continueGame == 'y') {
+			System.out.println("Please Enter Length:");
+			double length = scnr.nextDouble();
+			System.out.println("Please Enter Width:");
+			double width = scnr.nextDouble();
+
+			// I made functions for code reusability
+			System.out.println("Area: " + calculateArea(width, length));
+			System.out.println("Perimeter: " + calculatePerimeter(width, length));
+
+			System.out.println("Would you like to continue: (y/n)");
+			// for some reason nextChar() is not a command
+			continueGame = (scnr.next()).charAt(0);
+
+		}
+		System.out.println("Thanks!");
+
+	}
+
+	public static double calculateArea(double width, double length) {
+		double area = width * length;
+
+		return area;
+	}
+
+	public static double calculatePerimeter(double width, double length) {
+		double perimeter = (2 * width) + (2 * length);
+
+		return perimeter;
+	}
+
+}
